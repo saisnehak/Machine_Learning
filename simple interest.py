17c127d7d5a7132eeca8bb7152b5db35487c0079
saisnehak <snehakovuri2002@gmail.com>
2024-10-16 14:27:46 +0530

    simple interest.py

--- simple interest.py
+++ simple interest.py
@@ -1,7 +1,7 @@
 P=int(input("p:")) #principal amount
 T=int(input("T:")) #Time the money invested or borrowed for particular years
 R=int(input("R:")) #rate of interest per annum
-Simple_Interest= (P*T*R)/(100)   #simple interest
-Total_Amount=P+Simple_Interest             #Total interest
+Simple_Interest= (P*T*R)/(100)            #simple interest
+Total_Amount=P+Simple_Interest             #Total Amount
 print("Simple Interest:",Simple_Interest)
 print("Total Amount:",Total_Amount)
